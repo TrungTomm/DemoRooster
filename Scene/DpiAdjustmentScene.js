8524fed4ed75a0b1cf1b95ad3074114408fd469f
TrungTomm <dta11052003@gmail.com>
2024-10-15 14:08:09 +0700

    Updatedd

--- Scene/DpiAdjustmentScene.js
+++ Scene/DpiAdjustmentScene.js
@@ -11,24 +11,28 @@ export default class DpiAdjustmentScene extends Phaser.Scene {
     create() {
         this.adjustCanvasForDPI();
 
-        this.scene.start('MainScene');
         // Lắng nghe sự kiện resize để điều chỉnh lại canvas khi kích thước màn hình thay đổi
         window.addEventListener('resize', () => {
             this.adjustCanvasForDPI();
         });
+
+        this.scene.start('MainScene');
+
     }
 
     adjustCanvasForDPI() {
         let dpi = window.devicePixelRatio;
+        const width = Math.floor(window.innerWidth);
+        const height = Math.floor(window.innerHeight);
         let canvas = this.game.canvas;
 
-        // Điều chỉnh kích thước thật của canvas dựa trên DPI
-        canvas.width = this.game.scale.width * dpi;
-        canvas.height = this.game.scale.height * dpi;
+        // Điều chỉnh kích thước canvas dựa trên kích thước màn hình
+        canvas.width = width * dpi;
+        canvas.height = height * dpi;
 
         // Điều chỉnh lại kích thước hiển thị (CSS)
-        canvas.style.width = this.game.scale.width + "px";
-        canvas.style.height = this.game.scale.height + "px";
+        canvas.style.width = width + "px";
+        canvas.style.height = height + "px";
 
         // Điều chỉnh tỉ lệ của context
         let context = this.game.context;
