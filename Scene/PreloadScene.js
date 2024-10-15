00265a7ef9ee1053907de38662ceb506f948fcf6
TrungTomm <dta11052003@gmail.com>
2024-10-15 11:34:17 +0700

    Updated

--- Scene/PreloadScene.js
+++ Scene/PreloadScene.js
@@ -19,8 +19,6 @@ class PreloadScene extends Phaser.Scene {
     }
 
     create() {
-        const { width, height} = this.scale; 
-        console.log(width + "" + height)
         //Chuyển scene sang scene tiếp theo
         this.scene.start('MainScene');
         this.scene.run('moneyCount');
