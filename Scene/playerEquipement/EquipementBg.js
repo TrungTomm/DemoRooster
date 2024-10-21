11f938ba9b7728135926cd1f9dc0ca1801d11033
TrungTomm <dta11052003@gmail.com>
2024-10-21 15:35:59 +0700

    Updated

--- Scene/playerEquipement/EquipementBg.js
+++ Scene/playerEquipement/EquipementBg.js
@@ -9,8 +9,8 @@ class EquipementBg extends Phaser.Scene {
         const player_equipments_bg = this.add.nineslice(width / 2, 980, 'roosterbattle', 'player_equipments_bg',680,0,30,30);
         
         //Them Background cac o trang bi
-        // const player_eq_decor = this.add.image(width -300, height - 170, 'roosterbattle', 'player_equipments_decor');
-        // player_eq_decor.setScale(0.54);
+        const player_eq_decor = this.add.image(230, 960, 'roosterbattle', 'player_equipments_decor');
+
     }
     update() { }
 }
